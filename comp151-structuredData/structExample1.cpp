c5bf18e36cdd90e320c68b0210f2eb6a41ce4a38
Pedro Navarrete <pnavarrete@harding.edu>
2018-03-21 10:50:33 -0500

    Advances
    working with structure and streams

--- comp151-structuredData/structExample1.cpp
+++ comp151-structuredData/structExample1.cpp
@@ -6,6 +6,34 @@
 
 using namespace std;
 
+struct Comp151Stu {
+		string FirstName, LastName;
+		string degreeType, degreeMajor;
+		double GPA;
+	};
+
+
+void getStudentData (Comp151Stu &student, ifstream &stream){
+	stream >> student.FirstName >> student.LastName;
+};
+
 void main() {
 	
+	
+
+	ifstream stufile("comp151Roster.txt");
+
+	// Read the file data and store in my program
+	Comp151Stu aStudent;
+	
+	stufile >> aStudent.FirstName >> aStudent.LastName >> aStudent.degreeType >> aStudent.degreeMajor >> aStudent.GPA;
+
+	cout << "Here is the file data:" << endl << endl;
+	cout << "First Name: ";
+	cout << "Lat Name: ";
+	cout << "Degree Type: ";
+	cout << "Degree Major: ";
+	cout << "GPA: ";
+
+	stufile.close();
 }
