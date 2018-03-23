2d468c2a2cdd1f7d5eac408735242661968bf2b9
Pedro Navarrete <pnavarrete@harding.edu>
2018-03-23 10:49:31 -0500

    Finished X
    now it works with student arrays

--- comp151-structuredData/structExample1.cpp
+++ comp151-structuredData/structExample1.cpp
@@ -7,33 +7,56 @@
 using namespace std;
 
 struct Comp151Stu {
-		string FirstName, LastName;
-		string degreeType, degreeMajor;
-		double GPA;
-	};
-
-
-void getStudentData (Comp151Stu &student, ifstream &stream){
-	stream >> student.FirstName >> student.LastName;
+	string FirstName, LastName;
+	string degreeType, degreeMajor;
+	double GPA;
 };
 
-void main() {
-	
-	
+void ReadFile(Comp151Stu*, ifstream  &);
+void showStudentData(Comp151Stu*, ostream &);
 
+int main() {
+
+	const int MAXSTUDENTS = 50;
 	ifstream stufile("comp151Roster.txt");
 
 	// Read the file data and store in my program
-	Comp151Stu aStudent;
-	
-	stufile >> aStudent.FirstName >> aStudent.LastName >> aStudent.degreeType >> aStudent.degreeMajor >> aStudent.GPA;
+	Comp151Stu aStudent[MAXSTUDENTS];
 
-	cout << "Here is the file data:" << endl << endl;
-	cout << "First Name: ";
-	cout << "Lat Name: ";
-	cout << "Degree Type: ";
-	cout << "Degree Major: ";
-	cout << "GPA: ";
+	// Read student data
+	ReadFile(aStudent, stufile);
+
+	// Show student data
+	showStudentData(aStudent, cout);
 
 	stufile.close();
+}
+
+void ReadFile(Comp151Stu *student, ifstream &stream) {
+
+	int i = 0;
+	stream >> student[i].FirstName;
+
+	while (student[i].FirstName != "LAST") {
+		stream >> student[i].LastName >> student[i].degreeType >> student[i].degreeMajor >> student[i].GPA;
+		i++;
+		stream >> student[i].FirstName;
+	}
+
+}
+
+void showStudentData(Comp151Stu *student, ostream &f) {
+
+	f << "Here is the file data:" << endl << endl;
+
+	int i = 0;
+	while (student[i].FirstName != "LAST") {
+		f << "First Name           : " << student[i].FirstName << endl;
+		f << "Lat Name             : " << student[i].LastName << endl;
+		f << "Degree Type          : " << student[i].degreeType << endl;
+		f << "Degree Major         : " << student[i].degreeMajor << endl;
+		f << "GPA                  : " << student[i].GPA << endl;
+		f << endl;
+		i++;
+	}
 }
